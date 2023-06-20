28522b1328dc8c119a783077636671b0ef685935
朱森洋 <zhusenyang@deepinnet.com>
2023-06-20 14:08:24 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,68 @@
+import type { Plugin } from 'vite'
+import { asyncLoader, syncLoader } from './loadMethod'
+
+export interface AliyunARMSPluginOptions {
+  /**
+   * 是否启动插件，该选项优先级最高
+   * 
+   * @default true
+   */
+  enable?: boolean
+  /**
+   * 插件启动的场景
+   * 
+   * 未填写或undefined则所有场景都启用
+   * 
+   * @example 'build' 仅在Vite构建模式下使用
+   * 
+   * @default undefined
+   * 
+   * @see https://cn.vitejs.dev/guide/api-plugin.html#conditional-application
+   */
+  apply?: 'build' | 'serve'
+  /**
+   * 加载方式
+   * 
+   * @default 'async'
+   * 
+   * @description npm方式暂不可用
+   */
+  loadMethod?: 'async' | 'sync'
+  /**
+   * ARMS实例设置
+   * 
+   * @see https://help.aliyun.com/document_detail/58655.htm
+   */
+  configs: any
+
+}
+
+
+
+export default function plugin(options: AliyunARMSPluginOptions): Plugin {
+
+  const { enable = true, apply, loadMethod = 'async', configs } = options
+
+  if (!enable) {
+    return {
+      name: 'vite-plugin-aliyun-arms'
+    }
+  }
+
+  return {
+    name: 'vite-plugin-aliyun-arms',
+    enforce: 'pre',
+    apply: apply,
+    transformIndexHtml(html) {
+      switch (loadMethod) {
+        case 'async':
+          return asyncLoader(configs)
+        case 'sync':
+          return syncLoader(configs)
+        default:
+          return html
+      }
+    }
+  }
+}
+
---
+++ src/loadMethod.ts
@@ -0,0 +1,40 @@
+import { HtmlTagDescriptor } from "vite";
+
+export function asyncLoader(configs: any): HtmlTagDescriptor[] {
+  return [
+    {
+      tag: 'script',
+      injectTo: 'body-prepend',
+      children: `
+      !(function (c, b, d, a) {
+        c[a] || (c[a] = {})
+        c[a].config = ${JSON.stringify(configs)}
+        with (b) with (body) with (insertBefore(createElement('script'), firstChild)) setAttribute('crossorigin', '', (src = d))
+      })(window, document, 'https://retcode.alicdn.com/retcode/bl.js', '__bl')
+      `
+    }
+  ]
+}
+
+export function syncLoader(configs: any): HtmlTagDescriptor[] {
+  return [
+    {
+      tag: 'script',
+      injectTo: 'body-prepend',
+      children: `
+      window.__bl = {
+        config: ${JSON.stringify(configs)}
+      }
+      `
+    },
+    {
+      tag: 'script',
+      injectTo: 'body-prepend',
+      attrs: {
+        type: 'text/javascript',
+        crossorigin: true,
+        src: 'https://retcode.alicdn.com/retcode/bl.js'
+      }
+    }
+  ]
+}
